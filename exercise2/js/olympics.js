b3f37033b2e33dc9a4b80c9132e16b8f18025961
mas-dse-w9yan <w9yan@eng.ucsd.edu>
2018-01-21 22:22:06 -0800

    add colormap

--- exercise2/js/olympics.js
+++ exercise2/js/olympics.js
@@ -1,37 +1,62 @@
-function draw_stacked_bar(docid, columns, jsondata) {
+colormap_countries = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628'];
+colormap_sports = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628'];
+colormap_medals = ['gold','silver','#b87333'];
+colormap = [
+['#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b'],
+['#f7fcfd','#e0ecf4','#bfd3e6','#9ebcda','#8c96c6','#8c6bb1','#88419d','#810f7c','#4d004b'],
+['#f7f4f9','#e7e1ef','#d4b9da','#c994c7','#df65b0','#e7298a','#ce1256','#980043','#67001f'],
+['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'],
+['#fff7f3','#fde0dd','#fcc5c0','#fa9fb5','#f768a1','#dd3497','#ae017e','#7a0177','#49006a'],
+['#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#045a8d','#023858'],
+['#ffffff','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525','#000000'],
+];
+function draw_stacked_bar(docid, columns, jsondata, colormap) {
+  colormap_dict = {};
+  columns.forEach(function(col, i) { colormap_dict[col] = colormap[i]; });
   var screenwidth = $(window).width();
   var rotated = false;
   if(screenwidth < 800) {
     rotated = true;
   }
-	var chart = c3.generate({
-			bindto: docid,
-			data: {
-				json: jsondata,
-				type: 'bar',
-				keys: {
-					x: 'Country',
-					value: columns
-				},
-        groups: [columns ]
-			},
-			axis: {
-          rotated: rotated,
-					x: {
-							type: 'category',
-					}
+    var chart = c3.generate({
+        bindto: docid,
+        data: {
+            json: jsondata,
+            type: 'bar',
+            keys: {
+                x: 'Country',
+                value: columns
+            },
+            groups: [columns],
+            colors: colormap_dict,
+			color: function (color, d) {
+				if(d.id == undefined) {
+					return color;
+				}
+				col = d3.hsl(color)
+				//col.l += d.index * 0.1;  // increase luminance
+				//console.log(d.index, color, col);
+				//return col.brighter(d.index*0.2);
+				return col;
 			}
-	});
+        },
+        axis: {
+            rotated: rotated,
+            x: {
+                type: 'category',
+            }
+        }
+    });
 }
 
-function show_two_dimension(docid, years, categories, x_col, stack_col, csv_data) {
+function show_two_dimension(docid, years, categories, x_col, stack_col, csv_data, colormap) {
   var csv_data = csv_data.filter(function(r){
     return r.Year >= years[0] && r.Year<=years[1];
   });
 
   var data = d3.nest()
 		.key(function(d) { return d[x_col];}).sortValues(d3.ascending)
-		.key(function(d) { return d[stack_col];}).sortValues(d3.descending)
+		.key(function(d) { return d[stack_col];}).sortKeys(function(a,b) { return medals.indexOf(a) - medals.indexOf(b); })
 		.rollup(function(v) { 
 			return v.length;
 		}).entries(csv_data);
@@ -51,7 +76,7 @@ function show_two_dimension(docid, years, categories, x_col, stack_col, csv_data
   });
   rows.sort(function(a,b){ return b['total'] - a['total']; });
 
-  draw_stacked_bar(docid, categories, rows);
+  draw_stacked_bar(docid, categories, rows, colormap);
 }
 
 function get_column(csv_data, column) {
@@ -69,6 +94,11 @@ function get_column(csv_data, column) {
 }
 function draw_line_chart(docid, countries, jsondata) {
   var top_country_num = $('#select-country-count').val();
+  top_countries = countries.slice(0,top_country_num);
+
+  colormap_dict = {};
+  top_countries.forEach(function(col, i) { colormap_dict[col] = colormap_countries[i]; });
+
 	var chart = c3.generate({
 			bindto: docid,
 			data: {
@@ -79,6 +109,7 @@ function draw_line_chart(docid, countries, jsondata) {
           x: 'Year',
 					value: countries.slice(0,top_country_num),
         },
+        colors: colormap_dict,
 			},
 			axis: {
 					x: {
@@ -123,8 +154,8 @@ d3.csv("data/exercise2-olympics.csv", function(error, data) {
   countries = get_column(csv_data, 'Country');
 
   show_yearly_trend('#line-year-country', year_range, sports, countries, csv_data);
-  show_two_dimension('#barchart-country-sport', year_range, sports, 'Country', 'Sport', csv_data);
-  show_two_dimension('#barchart-country-medal', year_range, medals, 'Country', 'Medal', csv_data);
+  show_two_dimension('#barchart-country-sport', year_range, sports, 'Country', 'Sport', csv_data, colormap_sports);
+  show_two_dimension('#barchart-country-medal', year_range, medals, 'Country', 'Medal', csv_data, colormap_medals);
 });
 
 function update_charts(){
@@ -133,8 +164,8 @@ function update_charts(){
   });
   countries = get_column(data_in_years, 'Country');
   show_yearly_trend('#line-year-country', year_range, sports, countries, data_in_years);
-  show_two_dimension('#barchart-country-sport', year_range, sports, 'Country', 'Sport', data_in_years);
-  show_two_dimension('#barchart-country-medal', year_range, medals, 'Country', 'Medal', data_in_years);
+  show_two_dimension('#barchart-country-sport', year_range, sports, 'Country', 'Sport', csv_data, colormap_sports);
+  show_two_dimension('#barchart-country-medal', year_range, medals, 'Country', 'Medal', csv_data, colormap_medals);
 }
 function play(y) {
 	console.log('playing at year: ' + y);
