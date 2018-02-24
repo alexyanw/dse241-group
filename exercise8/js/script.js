50c75f9ef576f94281896e22a6ab9254d9376099
mas-dse-w9yan <w9yan@eng.ucsd.edu>
2018-02-23 21:55:42 -0800

    add exe8

---
+++ exercise8/js/script.js
@@ -0,0 +1,18 @@
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    county_boundary.resetStyle(e.target);
+}
