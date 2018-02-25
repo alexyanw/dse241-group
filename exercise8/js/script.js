d544f5eae46e321f8cedb40861a2292cbeec8481
mas-dse-w9yan <w9yan@eng.ucsd.edu>
2018-02-25 00:06:12 -0800

    update exe8

--- exercise8/js/script.js
+++ exercise8/js/script.js
@@ -16,3 +16,21 @@ function highlightFeature(e) {
 function resetHighlight(e) {
     county_boundary.resetStyle(e.target);
 }
+
+
+function check_county() {
+  var geo_counties = d3.map(all_counties, function(r) { return r.name; }).keys();
+  var counties = d3.map(virus_data, function(r) { return r.County; }).keys();
+  var missing = [];
+  for(var i in counties) {
+    var county = counties[i];
+    if(! geo_counties.includes(county)) {
+      missing.push(county);
+      console.log(county + ' is missing in geo boundaries');
+		}
+  }
+  console.log("total counties in virus data:", counties.length);
+  console.log("total missing in geo data:", missing.length);
+  return geo_counties;
+}
+
