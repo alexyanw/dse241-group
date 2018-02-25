573196f3491955d709f02b8ac03dc0d5f3b6d292
mas-dse-w9yan <w9yan@eng.ucsd.edu>
2018-02-25 14:36:27 -0800

    update exe8

--- exercise8/js/script.js
+++ exercise8/js/script.js
@@ -1,23 +1,3 @@
-function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-    }
-}
-
-function resetHighlight(e) {
-    county_boundary.resetStyle(e.target);
-}
-
-
 function check_county() {
   var geo_counties = d3.map(all_counties, function(r) { return r.name; }).keys();
   var counties = d3.map(virus_data, function(r) { return r.County; }).keys();
